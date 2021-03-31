84feb90b3a5f64f3dae6ef3a1272ba5a64e1fed5
vera <darthyoon@gmail.com>
2021-03-31 18:19:24 +0300

    Title, block. No adaptive.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
+import "./import-jquery"
+
 import './style/style.scss'
 console.log('ДОБРЫЙ ДЕНЬ')
+
+const catalog = document.querySelector('.catalog')
+const dropdown = document. querySelector('.catalog__dropdown')
+const triangle = document.querySelector('.triangle')
+const triangleActive = document.querySelector('.triangle_active')
+
+$(catalog).click(function() {
+  $(dropdown).toggle(500)
+  $(triangle).toggle()
+  $(triangleActive).toggle()
+});
+$(document).on('click', function(e) {
+  if (!$(e.target).closest(catalog).length) {
+    $(dropdown).hide(500)
+    $(triangleActive).hide()
+    $(triangle).show()
+  }
+  e.stopPropagation()
+});
