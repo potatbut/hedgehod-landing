d1e0cb0ea76e01736ced17cef0a01673dd85aee2
vera <darthyoon@gmail.com>
2021-04-04 21:35:34 +0300

    Added header and content block adaptation

--- src/index.js
+++ src/index.js
@@ -6,36 +6,40 @@ import 'slick-carousel/slick/slick.min';
 
 console.log('ДОБРЫЙ ДЕНЬ')
 
-const dropdown = document. querySelector('.catalog__dropdown')
-const triangle = document.querySelector('.triangle')
-const triangleActive = document.querySelector('.triangle_active')
-
-/* $(catalog).click(function() {
-  $(dropdown).toggle(500)
-  $(triangle).toggle()
-  $(triangleActive).toggle()
-});
-$(document).on('click', function(e) {
-  if (!$(e.target).closest(catalog).length) {
-    $(dropdown).hide(500)
-    $(triangleActive).hide()
-    $(triangle).show()
-  }
-  e.stopPropagation()
-});
- */
-
-$('.header__burger').click(function(event) {
-  $('.list__wrapper, .phone-dropdown, .header__burger').toggleClass("show")
-})
-
 $('.slider__wrapper').slick({
   infinite: true,
   slidesToShow: 2,
   slidesToScroll: 1,
   variableWidth: true,
+  responsive: [
+    {
+      breakpoint: 1440,
+      settings: {
+        slidesToShow: 1,
+      }
+    }, 
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 1,
+        arrows: false,
+      }
+    }, 
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        variableWidth: false,
+        arrows: false,
+      }
+    }
+  ]
 });
 
 $('.slide__info-badge').click(function(e) {
  $(e.target.querySelector(".info-block")).toggleClass("show")
+})
+
+$(".header__burger").click(function(event) {
+  $(".header__burger, .header__nav, .header__tel").toggleClass("display")
 })
